b0bb70518d52ff03824b8e2ecd0b4d4e79ed22c8
Madiyar <evs.mako@gmail.com>
2023-07-10 11:32:40 +0600

    changes in channel/models.py

--- channel/models.py
+++ channel/models.py
@@ -4,8 +4,8 @@ from user.models import User
 class Channel(models.model):
     channel_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
-    logo = models.ImageField()
-    background = models.ImageField()
-    description = models.TextField()
+    logo = models.ImageField(upload_to='images_uploaded', null=True)
+    background = models.ImageField(upload_to='images_uploaded', null=True)
+    description = models.TextField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
