230eab3f0ec33ed9ecfe68148be9277ff23d45e5
Madiyar <evs.mako@gmail.com>
2023-07-10 11:52:54 +0600

    declared comment/models.py

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from user.models import User
+from video.models import Video
 
-# Create your models here.
+class Comment(models.Model):
+    comment_id = models.IntegerField(primary_key=True)
+    comment_text= models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.ForeignKey(Video, on_delete=models.CASCADE)
+
