8380573bdd9ac569e1a4267a3bdaea1bf730f5e1
Madiyar <evs.mako@gmail.com>
2023-07-10 12:07:59 +0600

    declared watchHistory/models.py

--- watchHistory/models.py
+++ watchHistory/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from user.models import User
+from video.models import Video
 
-# Create your models here.
+class WatchHistory(models.Model):
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    watch_date = models.DateTimeField(auto_now_add=True)
+    video_id = models.ForeignKey(Video, on_delete=models.PROTECT)
+
