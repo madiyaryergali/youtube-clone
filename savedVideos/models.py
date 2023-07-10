91ddeeb8377770292926e0e2eeed13085257ce98
Madiyar <evs.mako@gmail.com>
2023-07-10 12:03:04 +0600

    declared savedVideos/models.py

--- savedVideos/models.py
+++ savedVideos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from user.models import User
+from video.models import Video
 
-# Create your models here.
+class SavedVideos(models.Model):
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
