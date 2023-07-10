d31de0695e82ea1d230247d821f3d4061683ac95
Madiyar <evs.mako@gmail.com>
2023-07-10 11:16:41 +0600

    declared user/models.py

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
