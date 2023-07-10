01a78aa57f1d21a344c385a9b61bbe6941032239
Madiyar <evs.mako@gmail.com>
2023-07-10 12:00:41 +0600

    declared playlist/models.py

--- playlist/models.py
+++ playlist/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from video.models import Video
+from user.models import User
 
-# Create your models here.
+class Playlist(models.Model):
+    playlist_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.ForeignKey(Video, on_delete=models.CASCADE)
