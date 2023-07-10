4703263cf496a89b183e809f936c22e31429da68
Madiyar <evs.mako@gmail.com>
2023-07-10 11:20:56 +0600

    declared channel/models.py

--- channel/models.py
+++ channel/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class Channel(models.model):
+    channel_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    logo = models.ImageField()
+    background = models.ImageField()
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
